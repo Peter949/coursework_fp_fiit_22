3c0180934582f106a20af500ab01399e70a63e1f
Mikhail.Starodubov <senseye51@gmail.com>
2025-03-01 12:17:31 +0900

    setup logger

--- telegram_bot/telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot/telegram_bot.py
@@ -4,31 +4,34 @@ import random
 from telegram import Update # type: ignore
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler # type: ignore
 
-resources = {}
+RESOURCES = {}
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
+logger = logging.getLogger(__name__);
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
+    logger.debug('start callback')
     await context.bot.send_message(chat_id=update.effective_chat.id, 
-                                   text=random.choice(resources['Greetings']))
+                                   text=random.choice(RESOURCES['Greetings']))
 
 async def end(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
+    logger.debug('end callback')
     await context.bot.send_message(chat_id=update.effective_chat.id, 
-                                   text=random.choice(resources['Bye']))
+                                   text=random.choice(RESOURCES['Bye']))
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level = logging.INFO)
     with open ('resources.json', encoding='utf-8') as f:
-        resources = json.load(f)
-    #logging.getLogger().log(' ' , resources)
-    print(resources)
-    token = open('token.txt', 'r', encoding='utf-8').read()
+        try:
+            RESOURCES = json.load(f)
+        except Exception as ex:
+            logger.error(f'can\'t load resources.json with error: {ex}')
+    try:
+        token = open('token.txt', 'r', encoding='utf-8').read()
+    except Exception as ex:
+        logger.error(f'can\'t open token file cause: ')
+    
+    pass
     application = ApplicationBuilder().token(token).build()
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
